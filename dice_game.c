f1f3b7e52451aabc70ea5c361a263446afabad03
ochiai nazuki <nazuki@wlan-napt-001.u-aizu.ac.jp>
2025-07-03 12:44:50 +0900

    Won/lost message added

--- dice_game.c
+++ dice_game.c
@@ -18,6 +18,13 @@ int main() {
     printf("Die 1: %d\n", die1);
     printf("Die 2: %d\n", die2);
     printf("Total value: %d\n", total);
+    
+    if (total > 7) {
+        printf("%s won!\n", name);
+    } else {
+        printf("%s lost.\n", name);
+    }
+
 
     return 0;
 }
